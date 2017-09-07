be077c972c36a31dcbbc0447df06e5ff3f86c5cb
Matheus Plessmann <maplessmann@gmail.com>
2017-09-07 11:20:28 -0300

    Add project folders and files

---
+++ gulpfile.js
@@ -0,0 +1,66 @@
+const gulp = require('gulp'),
+    pug = require('gulp-pug'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    wait = require('gulp-wait'),
+    imagemin = require('gulp-imagemin'),
+    uglify = require('gulp-uglify');
+
+
+/////////
+// PUG //
+/////////
+gulp.task('pug', function() {
+    return gulp.src('./src/pug/*.pug')
+        .pipe(pug({
+            pretty: true,
+        }))
+        .pipe(gulp.dest('./dist/'));
+});
+gulp.task('pug-watch', ['pug'], function() {
+    gulp.watch('./src/pug/**/*.pug', ['pug'])
+});
+
+
+//////////
+// SASS //
+//////////
+gulp.task('sass', function() {
+    return gulp.src('./src/sass/main.scss')
+        .pipe(wait(500))
+        .pipe(sass())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 4 versions', '> 5%', 'Firefox ESR']
+        }))
+        .pipe(gulp.dest('./dist/css/'));
+});
+gulp.task('sass-watch', ['sass'], function() {
+    gulp.watch('./src/sass/**/*.scss', ['sass'])
+});
+
+
+////////
+// JS //
+////////
+gulp.task('js', function() {
+    gulp.src('./src/js/main.js')
+        .pipe(gulp.dest('./dist/js/'));
+});
+gulp.task('js-watch', ['js'], function() {
+    gulp.watch('./src/js/**/*.js', ['js'])
+});
+
+
+////////////
+// IMAGES //
+////////////
+gulp.task('img', function() {
+    gulp.src(['./src/images/*.jpg', './src/images/*.png'])
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images/'));
+});
+
+
+// Default task
+gulp.task('default', ['pug-watch', 'sass-watch', 'js-watch', 'img']);
