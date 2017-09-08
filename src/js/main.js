76258c82a5ad2fb1259f49b6f93f2d7ae19c3cc9
Matheus Plessmann <maplessmann@gmail.com>
2017-09-08 14:13:26 -0300

    Add CSS animations

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,26 @@
 // Mobile menu
-const hamburger = document.querySelector('.hamburger-menu'),
-      mobileMenu = document.querySelector('.menu-mobile');
+(function() {
+    const hamburger = document.querySelector('.hamburger-menu'),
+          mobileMenu = document.querySelector('.menu-mobile');
+    
+    function toggleMenu() {
+        this.classList.toggle('active');
+        mobileMenu.classList.toggle('active');
+    }
+    
+    hamburger.addEventListener('click', toggleMenu);
+})();
 
-function toggleMenu() {
-    this.classList.toggle('active');
-    mobileMenu.classList.toggle('active');
-}
 
-hamburger.addEventListener('click', toggleMenu);
+/*
+ * Add 'loaded' class to the body tag after page load
+ */
+(function() {
+    const body = document.querySelector('body');
+
+    function loadAnimation() {
+        setTimeout(() => body.classList.add('loaded'), 100);
+    }
+
+    window.addEventListener('load', loadAnimation);
+})();
