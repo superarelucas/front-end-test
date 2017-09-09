2f653961d660f194e68b8ef3af8693e676fd89f8
kaueprata <kaue.p.b@hotmail.com>
2017-09-09 20:50:41 -0300

    Atualização do CSS Funcional e Finalização do Header

--- assets/projeto/index.php
+++ assets/projeto/index.php
@@ -4,17 +4,17 @@
   <title>Projeto de Teste da Superare</title>
   <meta name="description" content="Projeto criado por Kaue Prata"/>
   <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-beta/css/bootstrap-grid.min.css" rel="stylesheet" type="text/css"/>
-  <link href="css/style.css" rel="stylesheet" type="text/css"/>
+  <link href="css/style.css?1" rel="stylesheet" type="text/css"/>
 </head>
 <body>
   <section class="container-fluid" id="slide-01">
     <header class="row">
       <div class="container pad-lr-0">
         <div class="row mar-lr-0">
-          <div class="col-sm-12 col-xl-6"><a class="logo-font font-c-w font-w-r font-s-2 mar-tb-0" href="#">Brandi</a><span class="font-c-h2 font-w-l font-s-1 mar-lr-20">|</span><span class="font-c-w o-30 font-w-l font-s-1">I’am your tag line</span></div>
-          <div class="col-sm-12 col-xl-6">
+          <div class="col-xl-6 start-xl col-lg-6 start-lg col-md-5 center-md col-sm-12 center-sm center-xs"><a class="logo-font font-c-w font-w-r font-s-2 mar-tb-0" href="#">Brandi</a><span class="font-c-h2 font-w-l font-s-1 mar-lr-20">|</span><span class="font-c-w o-30 font-w-l font-s-1">I’am your tag line</span></div>
+          <div class="col-xl-6 end-xl col-lg-6 end-lg col-md-7 center-md col-sm-12 center-sm center-xs">
             <nav class="text-a-r">
-              <ul class="mar-tb-0 font-w-r font-s-1">
+              <ul class="mar-tb-0 font-w-r font-s-1 pad-l-0">
                 <li class="active"> <a href="#">Home</a></li>
                 <li><a href="#">Features</a></li>
                 <li><a href="#">Works</a></li>
