791bbd13ef5398d85ce914d7e287b525a87e44c4
kaueprata <kaue.p.b@hotmail.com>
2017-09-08 23:18:07 -0300

    Iniciação do projeto / Criação da estrutura dos arquivos e css funcional

---
+++ assets/projeto/index.php
@@ -0,0 +1,9 @@
+
+<html></html>
+<head>
+  <title>Projeto de Teste da Superare</title>
+  <meta name="description" content="Projeto criado por Kaue Prata"/>
+  <link href="css/style.css" rel="stylesheet" type="text/css"/>
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-beta/css/bootstrap-grid.min.css" rel="stylesheet" type="text/css"/>
+</head>
+<body></body>
