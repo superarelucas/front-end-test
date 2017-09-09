e2a6c29f56267c62a9d55e0a9687f08ec39cb847
kaueprata <kaue.p.b@hotmail.com>
2017-09-09 02:26:50 -0300

    Header adicionado

--- assets/projeto/index.php
+++ assets/projeto/index.php
@@ -3,7 +3,28 @@
 <head>
   <title>Projeto de Teste da Superare</title>
   <meta name="description" content="Projeto criado por Kaue Prata"/>
-  <link href="css/style.css" rel="stylesheet" type="text/css"/>
   <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-beta/css/bootstrap-grid.min.css" rel="stylesheet" type="text/css"/>
+  <link href="css/style.css" rel="stylesheet" type="text/css"/>
 </head>
-<body></body>
+<body>
+  <section class="container-fluid" id="slide-01">
+    <header class="row">
+      <div class="container pad-lr-0">
+        <div class="row mar-lr-0">
+          <div class="col-sm-12 col-xl-6"><a class="logo-font font-c-w font-w-r font-s-2 mar-tb-0" href="#">Brandi</a><span class="font-c-h2 font-w-l font-s-1 mar-lr-20">|</span><span class="font-c-w o-30 font-w-l font-s-1">I’am your tag line</span></div>
+          <div class="col-sm-12 col-xl-6">
+            <nav class="text-a-r">
+              <ul class="mar-tb-0 font-w-r font-s-1">
+                <li class="active"> <a href="#">Home</a></li>
+                <li><a href="#">Features</a></li>
+                <li><a href="#">Works</a></li>
+                <li><a href="#">Team</a></li>
+                <li><a href="#">Contact</a></li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </header>
+  </section>
+</body>
