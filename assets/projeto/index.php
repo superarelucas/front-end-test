1ac326ee2a20b7e6cfd3c9345ac7794a925fcc29
kaueprata <kaue.p.b@hotmail.com>
2017-09-10 14:17:42 -0300

    Layout Sessão Works Finalizado

--- assets/projeto/index.php
+++ assets/projeto/index.php
@@ -1,88 +1,170 @@
 
-<html></html>
-<head>
+<html>
   <title>Projeto de Teste da Superare</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   <meta name="description" content="Projeto criado por Kaue Prata"/>
   <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-beta/css/bootstrap-grid.min.css" rel="stylesheet" type="text/css"/>
   <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
-  <link href="css/style.css?1" rel="stylesheet" type="text/css"/>
-</head>
-<body>
-  <section class="container-fluid" id="slide-01">
-    <header class="row">
-      <div class="container pad-lr-0">
-        <div class="row mar-lr-0">
-          <div class="col-xl-6 start-xl col-lg-6 start-lg col-md-5 center-md col-sm-12 center-sm center-xs"><a class="logo-font font-c-w font-w-r font-s-2 mar-tb-0" href="#">Brandi</a><span class="font-c-h2 font-w-l font-s-1 mar-lr-20">|</span><span class="font-c-w o-30 font-w-l font-s-1">I’am your tag line</span></div>
-          <div class="col-xl-6 end-xl col-lg-6 end-lg col-md-7 center-md col-sm-12 center-sm center-xs">
-            <nav class="text-a-r">
-              <ul class="mar-tb-0 font-w-r font-s-1 pad-l-0">
-                <li class="active"> <a href="#">Home</a></li>
-                <li><a href="#">Features</a></li>
-                <li><a href="#">Works</a></li>
-                <li><a href="#">Team</a></li>
-                <li><a href="#">Contact</a></li>
-              </ul>
-            </nav>
+  <link href="css/style.css?16" rel="stylesheet" type="text/css"/>
+  <body>
+    <section class="container-fluid" id="slide-01">
+      <header class="row">
+        <div class="container pad-lr-0">
+          <div class="row mar-lr-0">
+            <div class="col-xl-6 start-xl col-lg-6 start-lg col-md-5 center-md col-sm-12 center-sm center-xs"><a class="logo-font font-c-w font-w-r font-s-2 mar-tb-0" href="#">Brandi</a><span class="font-c-h2 font-w-l font-s-1 mar-lr-20">|</span><span class="font-c-w o-30 font-w-l font-s-1">I’am your tag line</span></div>
+            <div class="col-xl-6 end-xl col-lg-6 end-lg col-md-7 center-md col-sm-12 center-sm center-xs">
+              <nav class="text-a-r">
+                <ul class="mar-tb-0 font-w-r font-s-1 pad-l-0">
+                  <li class="active"> <a href="#">Home</a></li>
+                  <li><a href="#">Features</a></li>
+                  <li><a href="#">Works</a></li>
+                  <li><a href="#">Team</a></li>
+                  <li><a href="#">Contact</a></li>
+                </ul>
+              </nav>
+            </div>
           </div>
+        </div>
+      </header>
+      <section class="row">
+        <div class="container-fluid">
+          <div class="row center-xs bottom-xs">
+            <div class="col-xs-12 col-lg-12 col-xl-12 title-slider">
+              <h1 class="font-s-4 font-w-l font-c-w mar-tb-0">Meet <span class="font-w-b">Brandi</span>!</h1>
+              <p class="font-s-2 font-w-l font-c-h mar-tb-0">/creative <span class="font-c-w">one page template.</span></p>
+              <p class="font-s-1 font-w-l font-c-h mar-tb-10">- <span class="font-c-w">We are a team of professionals </span>-</p>
+            </div>
+          </div>
+          <div class="social-buttons"><a class="social"><i class="fa fa-twitter" aria-hidden="true"></i></a><a class="social"><i class="fa fa-facebook" aria-hidden="true"></i></a><a class="social"><i class="fa fa-google-plus" aria-hidden="true"></i></a><a class="social"><i class="fa fa-dribbble" aria-hidden="true"></i></a></div>
+          <div class="slide-controler"><a class="active" slide-control="slide-01"></a><a slide-control="slide-01"></a><a slide-control="slide-01"></a></div>
+        </div>
+      </section>
+    </section>
+    <section class="container">
+      <div class="row">
+        <div class="col-12 center-xs">
+          <h1 class="font-c-h font-st-upper mar-t-70">Features</h1><span class="ornament font-s-1 font-c-h3"><i class="fa fa-heart-o" aria-hidden="true"></i></span>
         </div>
       </div>
-    </header>
-    <section class="row">
-      <div class="container-fluid">
-        <div class="row center-xs bottom-xs">
-          <div class="col-xs-12 col-lg-12 col-xl-12 title-slider">
-            <h1 class="font-s-4 font-w-l font-c-w mar-tb-0">Meet <span class="font-w-b">Brandi</span>!</h1>
-            <p class="font-s-2 font-w-l font-c-h mar-tb-0">/creative <span class="font-c-w">one page template.</span></p>
-            <p class="font-s-1 font-w-l font-c-h mar-tb-10">- <span class="font-c-w">We are a team of professionals </span>-</p>
+      <div class="row">
+        <div class="col-xs-12 col-xl-4 slide-item" slide-control="slide-01">
+          <div class="container-fluid pad-lr-0">
+            <div class="row mar-lr-0">
+              <div class="col-3 center-xs"><i class="fa fa-picture-o" aria-hidden="true"></i></div>
+              <div class="col-9">
+                <h1 class="font-c-blue font-w-sb mar-tb-0">Branding</h1>
+                <p class="font-c-blue font-s-1 o-70">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore</p>
+              </div>
+            </div>
           </div>
         </div>
-        <div class="social-buttons"><a class="social"><i class="fa fa-twitter" aria-hidden="true"></i></a><a class="social"><i class="fa fa-facebook" aria-hidden="true"></i></a><a class="social"><i class="fa fa-google-plus" aria-hidden="true"></i></a><a class="social"><i class="fa fa-dribbble" aria-hidden="true"></i></a></div>
-        <div class="slide-controler"><a class="active" slide-control="slide-01"></a><a slide-control="slide-01"></a><a slide-control="slide-01"></a></div>
+        <div class="col-xs-12 col-xl-4 slide-item" slide-control="slide-01">
+          <div class="container-fluid pad-lr-0">
+            <div class="row mar-lr-0">
+              <div class="col-3 center-xs"><i class="fa fa-pencil" aria-hidden="true"></i></div>
+              <div class="col-9">
+                <h1 class="font-c-blue font-w-sb mar-tb-0">Development</h1>
+                <p class="font-c-blue font-s-1 o-70">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-xs-12 col-xl-4 slide-item" slide-control="slide-01">
+          <div class="container-fluid pad-lr-0">
+            <div class="row mar-lr-0">
+              <div class="col-3 center-xs"><i class="fa fa-volume-down" aria-hidden="true"></i></div>
+              <div class="col-9">
+                <h1 class="font-c-blue font-w-sb mar-tb-0">Consulting</h1>
+                <p class="font-c-blue font-s-1 o-70">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="slide-controler-2 center-xs"><a class="active" slide-control="slide-01"></a><a slide-control="slide-01"></a><a slide-control="slide-01"></a></div>
       </div>
     </section>
-  </section>
-  <section class="container">
-    <div class="row center-xs">
-      <div class="col-xs-12 center-xs">
-        <h1 class="font-c-h font-st-upper mar-t-70">Features</h1><span class="ornament font-s-1 font-c-h3"><i class="fa fa-heart-o" aria-hidden="true"></i></span>
+    <section class="container">
+      <div class="row">
+        <div class="col-12 center-xs">
+          <h1 class="font-c-h font-st-upper mar-t-70">Works</h1><span class="ornament font-s-1 font-c-h3"><i class="fa fa-heart-o" aria-hidden="true"></i></span>
+        </div>
       </div>
-    </div>
-    <div class="row">
-      <div class="col-xs-12 col-xl-4 slide-item" slide-control="slide-01">
-        <div class="container-fluid pad-lr-0">
-          <div class="row mar-lr-0">
-            <div class="col-3 center-xs"><i class="fa fa-picture-o" aria-hidden="true"></i></div>
-            <div class="col-9">
-              <h1 class="font-c-blue font-w-sb mar-tb-0">Branding</h1>
-              <p class="font-c-blue font-s-1 o-70">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore</p>
+      <div class="row">
+        <div class="col-12 center-xs">
+          <p class="font-c-blue font-w-eb o-80">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore</p>
+        </div>
+      </div>
+    </section>
+    <section class="container-fluid">
+      <div class="row">
+        <nav class="col-12 center-xs">
+          <ul class="filter font-w-b font-c-blue center-xs">
+            <li class="active">All</li>
+            <li> Branding</li>
+            <li> Web</li>
+            <li> Logo Design</li>
+            <li> Photography</li>
+          </ul>
+        </nav>
+      </div>
+      <div class="row">
+        <div class="col-xs-12 col-sm-6 col-md-4 col-xl-3 pad-lr-0 bg-c-h item-works">
+          <div class="item-content content-1">
+            <div class="item-info center-xs middle-xs font-c-w"><i class="fa fa-eye" aria-hidden="true"></i>
+              <h2 class="font-w-b">Labore et dolore magnam</h2><span>Photography</span>
+            </div>
+          </div>
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-4 col-xl-3 pad-lr-0 bg-c-h item-works">
+          <div class="item-content content-2">
+            <div class="item-info center-xs middle-xs font-c-w"><i class="fa fa-eye" aria-hidden="true"></i>
+              <h2 class="font-w-b">Labore et dolore magnam</h2><span>Photography</span>
+            </div>
+          </div>
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-4 col-xl-3 pad-lr-0 bg-c-h item-works">
+          <div class="item-content content-3">
+            <div class="item-info center-xs middle-xs font-c-w"><i class="fa fa-eye" aria-hidden="true"></i>
+              <h2 class="font-w-b">Labore et dolore magnam</h2><span>Photography</span>
+            </div>
+          </div>
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-4 col-xl-3 pad-lr-0 bg-c-h item-works">
+          <div class="item-content content-4">
+            <div class="item-info center-xs middle-xs font-c-w"><i class="fa fa-eye" aria-hidden="true"></i>
+              <h2 class="font-w-b">Labore et dolore magnam</h2><span>Photography</span>
+            </div>
+          </div>
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-4 col-xl-3 pad-lr-0 bg-c-h item-works">
+          <div class="item-content content-5">
+            <div class="item-info center-xs middle-xs font-c-w"><i class="fa fa-eye" aria-hidden="true"></i>
+              <h2 class="font-w-b">Labore et dolore magnam</h2><span>Photography</span>
+            </div>
+          </div>
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-4 col-xl-3 pad-lr-0 bg-c-h item-works">
+          <div class="item-content content-6">
+            <div class="item-info center-xs middle-xs font-c-w"><i class="fa fa-eye" aria-hidden="true"></i>
+              <h2 class="font-w-b">Labore et dolore magnam</h2><span>Photography</span>
+            </div>
+          </div>
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-4 col-xl-3 pad-lr-0 bg-c-h item-works">
+          <div class="item-content content-7">
+            <div class="item-info center-xs middle-xs font-c-w"><i class="fa fa-eye" aria-hidden="true"></i>
+              <h2 class="font-w-b">Labore et dolore magnam</h2><span>Photography</span>
+            </div>
+          </div>
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-4 col-xl-3 pad-lr-0 bg-c-h item-works">
+          <div class="item-content content-8">
+            <div class="item-info center-xs middle-xs font-c-w"><i class="fa fa-eye" aria-hidden="true"></i>
+              <h2 class="font-w-b">Labore et dolore magnam</h2><span>Photography</span>
             </div>
           </div>
         </div>
       </div>
-      <div class="col-xs-12 col-xl-4 slide-item" slide-control="slide-01">
-        <div class="container-fluid pad-lr-0">
-          <div class="row mar-lr-0">
-            <div class="col-3 center-xs"><i class="fa fa-pencil" aria-hidden="true"></i></div>
-            <div class="col-9">
-              <h1 class="font-c-blue font-w-sb mar-tb-0">Development</h1>
-              <p class="font-c-blue font-s-1 o-70">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore</p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col-xs-12 col-xl-4 slide-item" slide-control="slide-01">
-        <div class="container-fluid pad-lr-0">
-          <div class="row mar-lr-0">
-            <div class="col-3 center-xs"><i class="fa fa-volume-down" aria-hidden="true"></i></div>
-            <div class="col-9">
-              <h1 class="font-c-blue font-w-sb mar-tb-0">Consulting</h1>
-              <p class="font-c-blue font-s-1 o-70">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore</p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="slide-controler-2 center-xs"><a class="active" slide-control="slide-01"></a><a slide-control="slide-01"></a><a slide-control="slide-01"></a></div>
-    </div>
-  </section>
-</body>
+    </section>
+  </body>
+</html>
