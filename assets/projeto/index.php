bf6ccd20765bbc65ee8a1996e5e99d46a67ac7e8
kaueprata <kaue.p.b@hotmail.com>
2017-09-09 22:50:56 -0300

    Layout Sessão Home Finalizado - Posicionamento e controlador do slider

--- assets/projeto/index.php
+++ assets/projeto/index.php
@@ -2,8 +2,10 @@
 <html></html>
 <head>
   <title>Projeto de Teste da Superare</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   <meta name="description" content="Projeto criado por Kaue Prata"/>
   <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-beta/css/bootstrap-grid.min.css" rel="stylesheet" type="text/css"/>
+  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
   <link href="css/style.css?1" rel="stylesheet" type="text/css"/>
 </head>
 <body>
@@ -26,5 +28,18 @@
         </div>
       </div>
     </header>
+    <section class="row">
+      <div class="container-fluid">
+        <div class="row center-xs bottom-xs">
+          <div class="col-xs-12 col-lg-12 col-xl-12 title-slider">
+            <h1 class="font-s-4 font-w-l font-c-w mar-tb-0">Meet <span class="font-w-b">Brandi</span>!</h1>
+            <p class="font-s-2 font-w-l font-c-h mar-tb-0">/creative <span class="font-c-w">one page template.</span></p>
+            <p class="font-s-1 font-w-l font-c-h mar-tb-10">- <span class="font-c-w">We are a team of professionals </span>-</p>
+          </div>
+        </div>
+        <div class="social-buttons"><a class="social" href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a><a class="social" href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a><a class="social" href="#"><i class="fa fa-google-plus" aria-hidden="true"></i></a><a class="social" href="#"><i class="fa fa-dribbble" aria-hidden="true"></i></a></div>
+        <div class="slide-controler"><a class="active" href="#" slide-control="slide-01"></a><a href="#" slide-control="slide-01"></a><a href="#" slide-control="slide-01"></a></div>
+      </div>
+    </section>
   </section>
 </body>
