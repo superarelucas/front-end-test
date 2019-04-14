62fc776cf97ddabaed24fdc4064926d632f41f56
victornns <victor.nascimento.ns@gmail.com>
2019-04-14 16:04:17 -0300

    created header component responsive

--- app/scripts/src/main.js
+++ app/scripts/src/main.js
@@ -11,4 +11,19 @@ window.addEventListener('DOMContentLoaded', function(event) {
         speed: 500
     });
 
+    var toggle = document.querySelector('.toggle-menu');
+    var header = document.querySelector('.header');
+    var modal = document.querySelector('.modal');
+    var body = document.body;
+
+    var open_menu = function(event) {
+        header.classList.toggle('open');
+        toggle.classList.toggle('is-active');
+        modal.classList.toggle('active');
+        body.classList.toggle('overflow-hidden');
+    }
+
+    toggle.addEventListener('click', function(event) { open_menu(event) })
+    modal.addEventListener('click', function(event) { open_menu(event) })
+
 });
