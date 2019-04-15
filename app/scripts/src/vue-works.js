a137ea3f45a8fbef6409046fb7ac008078ee8655
victornns <victor.nascimento.ns@gmail.com>
2019-04-15 17:05:04 -0300

    created interaction with vue js in component works

--- app/scripts/src/vue-works.js
+++ app/scripts/src/vue-works.js
@@ -4,7 +4,87 @@ window.addEventListener('DOMContentLoaded', function(event) {
         el: '#works',
         data() {
             return {
-                message: 'vue aqui'
+                categories: [
+                    {
+                        id: 'all',
+                        name: 'All'
+                    },
+                    {
+                        id: 'branding',
+                        name: 'Branding'
+                    },
+                    {
+                        id: 'web',
+                        name: 'Web'
+                    },
+                    {
+                        id: 'design',
+                        name: 'Design'
+                    },
+                    {
+                        id: 'photography',
+                        name: 'Photography'
+                    }
+                ],
+                active: 'all',
+                works: [
+                    {
+                        url: '#',
+                        title: 'Labore et dolore magnam',
+                        category: 'web',
+                        image: 'images/works-01.png',
+                        alt: 'Works example'
+                    },
+                    {
+                        url: '#',
+                        title: 'Interdum et malesuada fames',
+                        category: 'design',
+                        image: 'images/works-02.png',
+                        alt: 'Works example'
+                    },
+                    {
+                        url: '#',
+                        title: 'Etiam in metus',
+                        category: 'photography',
+                        image: 'images/works-03.png',
+                        alt: 'Works example'
+                    },
+                    {
+                        url: '#',
+                        title: 'Aliquam vehicula ante',
+                        category: 'branding',
+                        image: 'images/works-04.png',
+                        alt: 'Works example'
+                    },
+                    {
+                        url: '#',
+                        title: 'Labore et dolore magnam',
+                        category: 'photography',
+                        image: 'images/works-05.png',
+                        alt: 'Works example'
+                    },
+                    {
+                        url: '#',
+                        title: 'Labore et dolore magnam',
+                        category: 'branding',
+                        image: 'images/works-06.png',
+                        alt: 'Works example'
+                    },
+                    {
+                        url: '#',
+                        title: 'Phasellus imperdiet',
+                        category: 'photography',
+                        image: 'images/works-07.png',
+                        alt: 'Works example'
+                    },
+                    {
+                        url: '#',
+                        title: 'Sed quis dui massa',
+                        category: 'design',
+                        image: 'images/works-08.png',
+                        alt: 'Works example'
+                    }
+                ]
             }
         }
     })
