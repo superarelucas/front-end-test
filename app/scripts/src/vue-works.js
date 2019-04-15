663fd1e9982d6936ff94c37a29d732ed18ebb17a
victornns <victor.nascimento.ns@gmail.com>
2019-04-15 17:34:45 -0300

    corrected image path for deploy task

--- app/scripts/src/vue-works.js
+++ app/scripts/src/vue-works.js
@@ -32,57 +32,49 @@ window.addEventListener('DOMContentLoaded', function(event) {
                         url: '#',
                         title: 'Labore et dolore magnam',
                         category: 'web',
-                        image: 'images/works-01.png',
-                        alt: 'Works example'
+                        image: '../images/works-01.png',
                     },
                     {
                         url: '#',
                         title: 'Interdum et malesuada fames',
                         category: 'design',
-                        image: 'images/works-02.png',
-                        alt: 'Works example'
+                        image: '../images/works-02.png',
                     },
                     {
                         url: '#',
                         title: 'Etiam in metus',
                         category: 'photography',
-                        image: 'images/works-03.png',
-                        alt: 'Works example'
+                        image: '../images/works-03.png',
                     },
                     {
                         url: '#',
                         title: 'Aliquam vehicula ante',
                         category: 'branding',
-                        image: 'images/works-04.png',
-                        alt: 'Works example'
+                        image: '../images/works-04.png',
                     },
                     {
                         url: '#',
                         title: 'Labore et dolore magnam',
                         category: 'photography',
-                        image: 'images/works-05.png',
-                        alt: 'Works example'
+                        image: '../images/works-05.png',
                     },
                     {
                         url: '#',
                         title: 'Labore et dolore magnam',
                         category: 'branding',
-                        image: 'images/works-06.png',
-                        alt: 'Works example'
+                        image: '../images/works-06.png',
                     },
                     {
                         url: '#',
                         title: 'Phasellus imperdiet',
                         category: 'photography',
-                        image: 'images/works-07.png',
-                        alt: 'Works example'
+                        image: '../images/works-07.png',
                     },
                     {
                         url: '#',
                         title: 'Sed quis dui massa',
                         category: 'design',
-                        image: 'images/works-08.png',
-                        alt: 'Works example'
+                        image: '../images/works-08.png',
                     }
                 ]
             }
