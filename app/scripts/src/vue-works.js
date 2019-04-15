59ef3373e48f30f5f77dde9872cff4c11979d4e3
victornns <victor.nascimento.ns@gmail.com>
2019-04-15 14:31:21 -0300

    added vue js

---
+++ app/scripts/src/vue-works.js
@@ -0,0 +1,11 @@
+window.addEventListener('DOMContentLoaded', function(event) {
+
+    var works = new Vue({
+        el: '#works',
+        data() {
+            return {
+                message: 'vue aqui'
+            }
+        }
+    })
+})
