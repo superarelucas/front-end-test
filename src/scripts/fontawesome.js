e3dfa76bd13a489e279561c4875b85498679958f
Riva Junior <jrcavalcantejr@gmail.com>
2019-04-29 12:48:35 -0300

    fix: form appearance

--- src/scripts/fontawesome.js
+++ src/scripts/fontawesome.js
@@ -4,7 +4,6 @@
 import { library, dom } from '@fortawesome/fontawesome-svg-core'
 
 // Brands
-import { faInstagram } from '@fortawesome/free-brands-svg-icons/faInstagram'
 import { faFacebookF } from '@fortawesome/free-brands-svg-icons/faFacebookF'
 import { faTwitter } from '@fortawesome/free-brands-svg-icons/faTwitter'
 import { faGooglePlusG } from '@fortawesome/free-brands-svg-icons/faGooglePlusG'
@@ -17,23 +16,18 @@ import { faHeart } from '@fortawesome/free-regular-svg-icons/faHeart'
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons/faEnvelope'
 
 // Solids
-import { faPhone } from '@fortawesome/free-solid-svg-icons/faPhone'
-import { faChevronLeft } from '@fortawesome/free-solid-svg-icons/faChevronLeft'
-import { faChevronRight } from '@fortawesome/free-solid-svg-icons/faChevronRight'
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons/faArrowRight'
 
 library.add(
-  faInstagram,
   faFacebookF,
   faBehance,
   faTwitter,
   faGooglePlusG,
   faDribbble,
-  faPhone,
-  faChevronLeft,
-  faChevronRight,
   faEye,
   faHeart,
-  faEnvelope
+  faEnvelope,
+  faArrowRight
 )
 
 // Kicks off the process of finding <i> tags and replacing with <svg>
